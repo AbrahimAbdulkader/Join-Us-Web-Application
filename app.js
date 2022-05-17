77f2153edd21a1bb665a3aab0bc30cf2ccd056df
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-17 21:51:02 +0000

    Connected MySQL

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 var express = require('express'); 
+var mysql = require('mysql');
 var app = express(); 
 // We need to use 'require' express in order to use it
 
-
-
-app.get("/", function(req, res) {
-	res.send("You've Reached The Home Page!");
-	
+mysql = require('mysql') //We require the mysql package first 
+var connection = mysql.createConnection({ //Created the connection to the mysql package
+	host : 'localhost',
+	user : 'root',
+	database: 'join_us'
 });
-// Gets the request 
+
+
+
+app.get("/", function(req, res) { //Handles the get request
+	//Find count of users in DB
+	var q = "SELECT COUNT(*) AS count FROM users"; 
+	connection.query(q, function(error, results) { //Requests a connection to the DB 
+		if (error) throw error;
+		var count = results[0].count;
+			res.send("We have " + count + " users in our db") ;
+			//Respond with that count
+	});	
+});
 
 
 app.get("/joke", function(req, res) {
 	var joke = "What do you call a dog that does magic tricks? A labracadabrador."
 	res.send(joke);
 		});
+//Another request with '/joke' which takes us to another page and prints the joke string 
+
 
 app.listen(3000, function(){
 	console.log("Server running on 8080!")
