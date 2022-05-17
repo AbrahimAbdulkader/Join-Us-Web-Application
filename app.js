84d0107842f047b3317ca830b5dea94fa51750e8
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-17 17:15:31 +0000

    Get And Listen

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var express = require('express');
+var express = require('express'); 
 var app = express(); 
+// We need to use 'require' express in order to use it
+
+
+
+app.get("/", function(req, res) {
+	console.log("You've Reached The Home Page!")
+});
+
+app.listen(3000, function(){
+	console.log("Server running on 8080!")
+});
+// Bind and used to listen for the specified host and port
+// Sends a request
