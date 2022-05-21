5b19e91d113cf96da9a9d36cb380ff69905f532e
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-21 15:19:22 +0000

    Connected-The-Form

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express'); 
 var mysql = require('mysql');
-var app = express(); 
-// We need to use 'require' express in order to use it
+var app = express(); // We need to use 'require' express in order to use it
+var bodyParser = require('body-parser');
+
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true})); //Tell express that we are using body parser
 
 mysql = require('mysql') //We require the mysql package first 
 var connection = mysql.createConnection({ //Created the connection to the mysql package
@@ -11,6 +13,11 @@ var connection = mysql.createConnection({ //Created the connection to the mysql 
 	user : 'root',
 	database: 'join_us'
 });
+
+
+
+//-------------------------------------------------------------
+
 
 
 
@@ -26,6 +33,20 @@ app.get("/", function(req, res) { //Handles the get request
 	});	
 });
 
+//-------------------------------------------------------------
+
+
+app.post("/register", function(req, res) {
+	var person = {
+		email: req.body.email
+	};
+	connection.query('INSERT INTO users SET ?', person, function(error, results) {
+		if(error) throw error;
+		res.send("Thanks for joining the server")
+	});
+});
+
+
 
 app.get("/joke", function(req, res) {
 	var joke = "<strong>What do you call a dog that does magic tricks?</strong><em>A labracadabrador.</em>"
@@ -35,8 +56,21 @@ app.get("/joke", function(req, res) {
 //Another request with '/joke' which takes us to another page and prints the joke string 
 
 
+//-------------------------------------------------------------
+
+
+
+
+
 app.listen(3000, function(){
 	console.log("Server running on 8080!")
 });
 // Bind and used to listen for the specified host and port
 // Sends a request   
+
+
+//-------------------------------------------------------------
+
+
+
+
